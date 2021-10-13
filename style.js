17c58cb26834052dda0ba84540c30a67a8ba787b
Hendfawaz <Hend.fawaz.ruh.js@tuwaiq.edu.sa>
2021-10-13 13:23:12 +0300

    MStor

---
+++ style.js
@@ -0,0 +1,31 @@
+// let shoppingCart = function() {
+//     cart = [];
+// }
+// function Item(material , price, ) {
+//         this.material = material;
+//         this.price = price;
+// }
+
+// function saveCart() {
+//  sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+
+
+
+// if (sessionStorage.getItem("shoppingCart") != null) {
+//     loadCart();
+//   }
+//   var obj ={};
+//   obj.addItemToCart = function(material, price, ) {
+//     for(var item in cart) {
+//       if(cart[item].material === material) {
+//         cart[item].count ++;
+//         saveCart();
+//         return;
+//       }}
+//    var item = new Item(material, price,) ;
+//    cart.push(item);
+//    saveCart();
+//   }
+// }
+
+
